531d5ea98c719176a1b51bd154c0ac0ff80c390d
kamrankhowaja <kamrankhowaja@yahoo.com>
2020-07-20 12:36:55 +0500

    Done

--- Class1/Assignmnet 5.py
+++ Class1/Assignmnet 5.py
@@ -1,8 +1,14 @@
 #Star printing
-
+print("Task 1")
 a =int(input("Enter number :"))
 
-for i in range(1, a):
-    for j in range(1,a-i):
+for i in range(1,a+1):
+    for j in range(0,a-i):
         print(" " , end="")
     print(str("*")*i)
+
+print("Task 2")
+for f in range(0,a):
+    for g in range(0,a-f):
+        print("*",end=" ")
+    print()
